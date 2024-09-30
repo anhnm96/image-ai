660e048b04b84d3e33797922267df2e522d0b48e
Nguyen Minh Anh <xiaosasori@gmail.com>
2024-09-30 22:21:28 +0700

    init

---
+++ app/app.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+  <div>
+    <h2 class="text-green-500 text-xl">
+      Hello
+    </h2>
+    <UiButton variant="default">
+      UI Button
+    </UiButton>
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,26 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: false },
+  future: {
+    compatibilityVersion: 4,
+  },
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  css: ['~/assets/main.css'],
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './components/ui',
+  },
+})
